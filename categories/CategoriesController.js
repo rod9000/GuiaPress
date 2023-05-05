53ab5f242d402d5d7b9b81b199415cd0068c5020
Rodrigo <71358562+rod9000@users.noreply.github.com>
2023-05-05 07:41:49 -0300

    Criação e Exclusao de categoria
    adicionado nova pagina para criação listagem e exclusão de categorias

--- categories/CategoriesController.js
+++ categories/CategoriesController.js
@@ -1,12 +1,49 @@
 const express = require("express");
 const router = express.Router();
-
-router.get("/categories", (req, res) => {
-    res.send("ROTA DE CATEGORIAS");
-});
+const Category = require("./Category");
+const slugify = require("slugify");
 
 router.get("/admin/categories/new", (req, res) => {
-    res.send("ROTA PARA CRIAR UMA NOVA CATEGORIA!");
+     res.render("admin/categories/new");
+});
+
+router.post("/categories/save", (req, res) => {
+    var title = req.body.title;
+    if(title != undefined){
+        Category.create({
+            title: title,
+            slug: slugify(title)
+        }).then(() => {
+            res.redirect("/admin/categories/");
+        });
+    }else{
+        res.redirect("/admin/categories/new");
+    }
+});
+
+router.get("/admin/categories", (req, res) => {
+    Category.findAll().then(categories =>{
+        res.render("admin/categories/index", {categories : categories});
+    });
+});
+
+router.post("/categories/delete", (req, res) =>{
+    var id = req.body.id;
+    if(id != undefined){
+        if(!isNaN(id)){
+            Category.destroy({
+                where:{
+                    id: id
+                }
+            }).then(() => {
+                res.redirect("/admin/categories");
+            });
+        }else{
+            res.redirect("/admin/categories");
+        }
+    }else{
+        res.redirect("/admin/categories");
+    }
 });
 
 module.exports = router;
