b648ce8426226fde5d417943960ef7774a9542ff
Rodrigo <71358562+rod9000@users.noreply.github.com>
2023-05-08 16:45:19 -0300

    Adicionado TinyMCE
    Adicionado cadastro de titulos e Tiny MC

--- articles/ArticlesController.js
+++ articles/ArticlesController.js
@@ -1,12 +1,34 @@
 const express = require("express");
 const router = express.Router();
+const Category = require("../categories/Category");
+const Article = require("./Article");
+const slugify = require("slugify");
 
-router.get("/articles", (req, res) => {
+router.get("/admin/articles", (req, res) => {
     res.send("ROTA DE ARTIGOS");
 });
 
 router.get("/admin/articles/new", (req, res) => {
-    res.send("ROTA PARA CRIAR UM NOVO ARTIGO!");
+    Category.findAll().then(categories => {
+        console.log(categories);
+        res.render("admin/articles/new", {categories : categories});
+    });
+});
+
+router.post("/articles/save", (req, res) => {
+    var title = req.body.title;
+    var body = req.body.body;
+    var category = req.body.category;
+ 
+    Article.create({
+        title: title, 
+        slug: slugify(title),
+        body : body, 
+        categoryId: category
+    }).then(() => {
+        res.redirect("/admin/articles");
+    })
+
 });
 
 module.exports = router;
--- database/database.js
+++ database/database.js
@@ -2,7 +2,8 @@ const Sequelize = require("sequelize");
 
 const connection = new Sequelize("guiapress", "root", "root",{
     host: '127.0.0.1',
-    dialect: 'mysql'
+    dialect: 'mysql',
+    timezone: "-03:00"
 });
 
 module.exports = connection;
